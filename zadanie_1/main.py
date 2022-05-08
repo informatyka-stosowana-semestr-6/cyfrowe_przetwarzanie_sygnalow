c3f5e41e0addd3fe7d8d41f206b71b74cfc1ffc9
balnabu <przemek_lis@onet.eu>
2022-05-08 14:21:45 +0200

    1/2 działa

--- zadanie_1/main.py
+++ zadanie_1/main.py
@@ -1,7 +1,77 @@
+import sys
+
+import numpy as np
+
+from zadanie_1.gui.gui import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
 from zadanie_1.signal import Signal
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
-s = Signal()
 
-plt.plot(s._gaussian_noise(10, 0, 100))
-plt.show()
 
+if __name__ == "__main__":
+
+    def create_chart(signal_value):
+        plt.figure()
+
+        if signal_value == results:
+            signal_value.MATH[ui.comboBox_3.currentText()](signal_1, signal_2)
+
+        if signal_value == signal_1:
+            if ui.comboBox.currentText() == "UNIT_IMPULSE" or ui.comboBox.currentText() == "IMPULSE_NOISE":
+                plt.scatter(signal_value.x_values, signal_value.y_values)
+            else:
+                plt.plot(signal_value.x_values, signal_value.y_values)
+            plt.savefig("signal_1.png", dpi=300)
+            ui.label_12.setPixmap(QtGui.QPixmap("signal_1.png"))
+        elif signal_value == signal_2:
+            if ui.comboBox_2.currentText() == "UNIT_IMPULSE" or ui.comboBox.currentText() == "IMPULSE_NOISE":
+                plt.scatter(signal_value.x_values, signal_value.y_values)
+            else:
+                plt.plot(signal_value.x_values, signal_value.y_values)
+            plt.savefig("signal_2.png", dpi=300)
+            ui.label_13.setPixmap(QtGui.QPixmap("signal_2.png"))
+        else:
+            # if ui.comboBox.currentText() == "UNIT_IMPULSE" or ui.comboBox.currentText() == "IMPULSE_NOISE":
+            #     plt.scatter(signal_value.x_values, signal_value.y_values)
+            # else:
+            plt.plot(signal_value.x_values, signal_value.y_values)
+            plt.savefig("signal_result.png", dpi=300)
+            ui.label_14.setPixmap(QtGui.QPixmap("signal_result.png"))
+
+    def update_values():
+        signals = [signal_1, signal_2, results]
+        for sig in signals:
+            sig.A = float(ui.lineEdit.text())
+            sig.t1 = float(ui.lineEdit_2.text())
+            sig.d = float(ui.lineEdit_3.text())
+            sig.T = float(ui.lineEdit_4.text())
+            sig.kw = float(ui.lineEdit_5.text())
+            sig.jump_time = float(ui.lineEdit_6.text())
+            sig.possibility = float(ui.lineEdit_7.text())
+            sig.freq = float(ui.lineEdit_8.text())
+            if sig == signal_1:
+                sig.generate(ui.comboBox.currentText())
+            elif sig == signal_2:
+                sig.generate(ui.comboBox_2.currentText())
+            # plot
+            create_chart(sig)
+            # save
+            # sig.save_to_file(signal_1, ui.comboBox.currentText())
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    signal_1 = Signal()
+    signal_2 = Signal()
+    results = Signal()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.comboBox.addItems(signal_1.all_signals.keys())
+    ui.comboBox_2.addItems(signal_1.all_signals.keys())
+    ui.comboBox_3.addItems(["Dodawanie", "Odejmowanie", "Mnożenie", "Dzielenie"])
+    ui.pushButton.clicked.connect(update_values)
+
+
+    MainWindow.show()
+    sys.exit(app.exec_())
--- zadanie_1/signal.py
+++ zadanie_1/signal.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import random
 import math
 
 
@@ -6,25 +7,29 @@ class Signal:
     ##########################
     # CONSTANT
     ##########################
-    _UNIT_NOISE_DISTRIBUTION = "UNIT_NOISE_DISTRIBUTION"  # S1
-    _GAUSSIAN_NOISE = "GAUSSIAN_NOISE"
-    _SINUSOIDAL_NOISE = "SINUSOIDAL_NOISE"
-    _ONE_HALF_RECTIFIED_SINUSOIDAL_SIGNAL = "ONE_HALF_RECTIFIED_SINUSOIDAL_SIGNAL"
-    _TWO_HALF_RECTIFIED_SINUSOIDAL_SIGNAL = "TWO_HALF_RECTIFIED_SINUSOIDAL_SIGNAL"
-    _SQUARE_WAVE_SIGNAL = "SQUARE_WAVE_SIGNAL"
-    _SYMMETRICAL_SQUARE_WAVE_SIGNAL = "SYMMETRICAL_SQUARE_WAVE_SIGNAL"
-    _TRIANGULAR_SIGNAL = "TRIANGULAR_SIGNAL"
-    _UNIT_JUMP = "UNIT_JUMP"
-    _UNIT_IMPULSE = "UNIT_IMPULSE"
-    _IMPULSE_NOISE = "IMPULSE_NOISE"
+
 
     def __init__(self):
         self.all_signals = self._generate_dict_with_all_signals()
+        self.MATH = self._generate_math()
+        self.x_values = None
+        self.y_values = None
+        self.A = None
+        self.T = None
+        self.t1 = None
+        self.d = None
+        self.kw = None
+        self.jump_time = None
+        self.possibility = None
+        self.freq = 60
+
+    def set_x(self):
+        self.x_values = np.linspace(float(self.t1), float(self.t1) + float(self.d), int(float(self.d) * float(self.freq)))
 
     def _generate_dict_with_all_signals(self) -> dict:
         return {"UNIT_NOISE_DISTRIBUTION": self._unit_noise_distribution,
                 "GAUSSIAN_NOISE": self._gaussian_noise,
-                "SINUSOIDAL_NOISE": self._sinusoidal_noise,
+                "SINUSOIDAL_SIGNAL": self._sinusoidal_signal,
                 "ONE_HALF_RECTIFIED_SINUSOIDAL_SIGNAL": self._one_half_rectified_sinusoidal_signal,
                 "TWO_HALF_RECTIFIED_SINUSOIDAL_SIGNAL": self._two_half_rectified_sinusoidal_signal,
                 "SQUARE_WAVE_SIGNAL": self._square_wave_signal,
@@ -34,40 +39,120 @@ class Signal:
                 "UNIT_IMPULSE": self._unit_impulse,
                 "IMPULSE_NOISE": self._impulse_noise}
 
-    def _unit_noise_distribution(self, A, t1, d) -> np.array:
-        noise = np.array([np.random.uniform(low=-A, high=A, size=None) for _ in range(t1, d)])
-        return noise
+    def _generate_math(self):
+        return {"Dodawanie": self.add, "Odejmowanie": self.subtraction, "Mnożenie": self.multiply, "Dzielenie": self.division}
 
-    def _gaussian_noise(self, A, t1, d) -> np.array:
-        noise = np.array([(1/math.sqrt(2*math.pi)) * pow(math.e, (-pow(np.random.uniform(low=-A, high=A, size=None), 2)) / 2) for _ in range(t1, d)])
-        return noise
+    def _unit_noise_distribution(self) -> np.array:
+        self.y_values = np.array((random.uniform(-self.A, self.A, size=(len(self.x_values),))).tolist())
 
-    def _sinusoidal_noise(self):
-        pass
+    def _gaussian_noise(self) -> np.array:
+        self.y_values = np.array((random.normal(scale=self.A, size=(len(self.x_values),))).tolist())
+
+    def _sinusoidal_signal(self):
+        self.y_values = self.A * np.sin((2 * np.pi / self.T) * (self.x_values - self.t1))
 
     def _one_half_rectified_sinusoidal_signal(self):
-        pass
+        self.y_values = 0.5 * self.A * (np.sin((2 * np.pi / self.T) * (self.x_values - self.t1)) + np.abs(np.sin((2 * np.pi / self.T) * (self.x_values - self.t1))))
 
     def _two_half_rectified_sinusoidal_signal(self):
-        pass
+        self.y_values = self.A * np.abs(np.sin((2 * np.pi / self.T) * (self.x_values - self.t1)))
 
     def _square_wave_signal(self):
-        pass
+        values = []
+        for x in self.x_values:
+            if (((x - self.t1) % self.T) / self.T) < self.kw:
+                values.append(self.A)
+            else:
+                values.append(0)
+        self.y_values = values
 
     def _symmetrical_square_wave_signal(self):
-        pass
+        values = []
+        for x in self.x_values:
+            if (((x - self.t1) % self.T) / self.T) < self.kw:
+                values.append(self.A)
+            else:
+                values.append(-self.A)
+        self.y_values = values
 
     def _triangular_signal(self):
-        pass
+        values = []
+        for x in self.x_values:
+            if (((x - self.t1) % self.T) / self.T) < self.kw:
+                values.append((self.A / (self.kw * self.T)) * ((x - self.t1) % self.T))
+            else:
+                values.append((-self.A / (self.T * (1 - self.kw)) * ((x - self.t1) % self.T)) + self.A / (1 - self.kw))
+        self.y_values = values
+
+        # alpha = (self.A) / (self.d / 2)
+        # self.y_values = -self.A / 2 + self.A * ((self.x_values - 1/self.T) % self.d == self.d / 2) + alpha * \
+        #        ((self.x_values - 1/self.T) % (self.d / 2)) * ((self.x_values - 1/self.T) % self.d <= self.d / 2) \
+        #        + (self.A - alpha * ((self.x_values - 1/self.T) % (self.d / 2))) * ((self.x_values - 1/self.T) % self.d > self.d / 2)
 
     def _unit_jump(self):
-        pass
+        values = []
+        for x in self.x_values:
+            if x < self.jump_time:
+                values.append(0)
+            elif x == self.jump_time:
+                values.append(0.5 * self.A)
+            else:
+                values.append(self.A)
+        self.y_values = values
 
     def _unit_impulse(self):
-        pass
+        values = []
+        for i in range(self.x_values.size - 1):
+            if self.x_values[i] <= self.jump_time < self.x_values[i + 1]:
+                values.append(self.A)
+            else:
+                values.append(0)
+        values.append(0)
+        self.y_values = values
 
     def _impulse_noise(self):
-        pass
+        values = []
+        for x in self.x_values:
+            if random.uniform(0, 1) < self.possibility:
+                values.append(self.A)
+            else:
+                values.append(0)
+        self.y_values = values
+
+    def add(self, s1, s2):
+        values = []
+        for i in range(s1.x_values.size):
+            values.append(s1.y_values[i] + s2.y_values[i])
+        self.x_values = s1.x_values
+        self.y_values = values
+
+    def subtraction(self, s1, s2):
+        values = []
+        for i in range(s1.x_values.size):
+            values.append(s1.y_values[i] - s2.y_values[i])
+        self.x_values = s1.x_values
+        self.y_values = values
+
+    def multiply(self, s1, s2):
+        values = []
+        for i in range(s1.x_values.size):
+            values.append(s1.y_values[i] * s2.y_values[i])
+        self.x_values = s1.x_values
+        self.y_values = values
+
+    def division(self, s1, s2):
+        values = []
+        for i in range(s1.x_values.size):
+            values.append(s1.y_values[i] / s2.y_values[i])
+        self.x_values = s1.x_values
+        self.y_values = values
 
     def generate(self, signal: str):
-        return self.all_signals[signal]()
+        self.set_x()
+        self.all_signals[signal]()
+
+    def save_to_file(self, file_name, signal):
+        with open(file_name, "w") as file:
+            file.write(f"signal = {signal}")
+            file.write(f"x = {self.x_values}")
+            file.write(f"y = {self.y_values}")
